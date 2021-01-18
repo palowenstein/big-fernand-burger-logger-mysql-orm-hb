d6dee1ba5a152c1493401d539ff98e11f04de72a
abbotkinneydude <pa@soundtrackspecialist.com>
2021-01-18 11:35:02 -0800

    seed sql fix

--- db/seed.sql
+++ db/seed.sql
@@ -1,13 +1,12 @@
-
 -- Requires the bigFernand database (schema).
 USE bigFernand_db;
 
--- Insert 6 burger choices from Big Fernand in the Burgers table: 1) burger_name, 2) eaten_burger (default false if eaten_burger left blank)
-INSERT INTO burgers (burger_name, eaten_burger) VALUES ('LE BIG FERNAND');
-INSERT INTO burgers (burger_name, eaten_burger) VALUES ('LE BARTHOLOMÉ');
-INSERT INTO burgers (burger_name, eaten_burger) VALUES ('LE BASILE');
-INSERT INTO burgers (burger_name, eaten_burger) VALUES ('LE PAULIN');
-INSERT INTO burgers (burger_name, eaten_burger) VALUES ('LE VICTOR');
+-- Insert 6 burger choices from Big Fernand in the Burgers table: 1) burger_name, 2) eaten_burger (boolean - default false, only included if true)
+INSERT INTO burgers (burger_name) VALUES ('LE BIG FERNAND');
+INSERT INTO burgers (burger_name) VALUES ('LE BARTHOLOMÉ');
+INSERT INTO burgers (burger_name) VALUES ('LE BASILE');
+INSERT INTO burgers (burger_name) VALUES ('LE PAULIN');
+INSERT INTO burgers (burger_name) VALUES ('LE VICTOR');
 INSERT INTO burgers (burger_name, eaten_burger) VALUES ('LE LUCIEN (VÉGÉTARIEN)', true);
 
 -- #### DB Setup
