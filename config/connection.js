a76a3e2148cf65bbc1fab3ec34228975d0b6cabe
abbotkinneydude <pa@soundtrackspecialist.com>
2021-01-21 23:00:32 -0800

    updated connection, orm, new port, variables separation

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,25 @@
-// Set up MySQL connection (network1).
+// Set up MySQL connection (networkOne).
 const mysql = require("mysql");
 
-let network1 = mysql.createConnection({
+let networkOne = mysql.createConnection({
     host: "localhost",
-    port: 4000,
+    port: 3306,
     user: "root",
-    password: "Margaux0228",
-    database: "bigFernand_db;"
+    password: "",
+    database: "bigFernand_db"
   });
   
-  // Make connection (network1).
-  network1.connect(function(err) {
+  // Make connection (networkOne).
+  networkOne.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
       return;
     }
-    console.log("connected as id " + network1.threadId);
+    console.log("connected as id " + networkOne.threadId);
   });
   
-  // Export connection (network1) for our ORM to use.
-  module.exports = network1;
+  // Export connection (networkOne) for our ORM to use.
+  module.exports = networkOne;
   
 // Note: `let`, is a signal that the variable may be reassigned, such as a counter in a loop, or a value swap in an algorithm.
 // It also signals that the variable will be used only in the block it’s defined in, which is not always the entire containing function.
